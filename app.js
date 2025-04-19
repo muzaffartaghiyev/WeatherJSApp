f2e18efea12f7bae5da9abbb667f9368128b6e35
Muzaffar <75939608+muzeffertagiyev@users.noreply.github.com>
2025-04-19 20:28:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const locationDiv = document.getElementById("userLocation")
 let userLocation = false //for sending user location to left side
 
 // ! Variables
-const API_KEY = "e17c897aa935921a081cf75bcd4fb9e2"
+// const API_KEY = "e17c897aa935921a081cf75bcd4fb9e2"
+const API_KEY = ${{ secrets.API_KEY }}
 let url;
 let unit = 'metric'
 
